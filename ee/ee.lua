55f2f0e32d7d53fa106d161f214f26760d3c3818
triple7distro <triple7distro@proton.me>
2026-04-26 19:32:37 +0200

    Update ee.lua

--- ee/ee.lua
+++ ee/ee.lua
@@ -1,1 +1,4 @@
-
+wait(1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/triple7distro/triple7/refs/heads/main/ee/hp.lua"))()
+wait(1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/triple7distro/triple7/refs/heads/main/ee/aim.lua"))()
