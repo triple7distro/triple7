efafba897e23d088582d52ed219f95ca55493c55
triple7distro <triple7distro@proton.me>
2026-04-27 00:09:47 +0200

    Update light.lua

--- ee/light.lua
+++ ee/light.lua
@@ -1,24 +1,24 @@
 local lgbt_1 = game:GetService("Lighting")
 
-lgbt_1.GlobalShadows = false
+lgbt_1.GlobalShadows = true
 lgbt_1.Ambient = Color3.new(1, 1, 1)
 
 if lgbt_1:FindFirstChild("Bloom") then
-    lgbt_1.Bloom.Enabled = false
-    lgbt_1.Bloom.Intensity = 0
+    lgbt_1.Bloom.Enabled = true
+    lgbt_1.Bloom.Intensity = 100
 end
 if lgbt_1:FindFirstChild("Blur") then
     lgbt_1.Blur.Enabled = false
     lgbt_1.Blur.Size = 0
 end
 if lgbt_1:FindFirstChild("ColorCorrection") then
-    lgbt_1.ColorCorrection.Enabled = false
+    lgbt_1.ColorCorrection.Enabled = true
 end
 if lgbt_1:FindFirstChild("SunRays") then
-    lgbt_1.SunRays.Enabled = false
+    lgbt_1.SunRays.Enabled = true
 end
 if lgbt_1:FindFirstChild("Sky") then
-    lgbt_1.Sky.Enabled = false
+    lgbt_1.Sky.Enabled = true
 end
 
 lgbt_1.FogEnd = 100000
